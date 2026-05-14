d75a8bea5518319dac6aba0bc4d46fda4fbbdaef
Herpsime <negarse63@gmail.com>
2026-05-14 18:00:51 +0300

    done the first activity

---
+++ Python/day 1/activity 1.py
@@ -0,0 +1,15 @@
+#Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
+print('int', -454663)
+print('float', 2.76)
+print ('complex number', complex(3, 2) )
+print('string','hersime')
+print('boolean',(False))
+print('list', [5,6,7,8,9])
+print('dictionary', {
+    'name':'Herpsime',
+    'country':'ethiopia',
+    'age':1234,
+    'b.p':118.4
+})
+print('tuple',('monday','tuesday','wednesday','thursday','friday','saturday','sunday'))
+print('set',{8,2,6,9})
