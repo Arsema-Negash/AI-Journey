e58c422f2c8411a57c2150d4e4fb621de56e85d6
Herpsime <negarse63@gmail.com>
2026-05-15 17:25:42 +0300

    finished  day2 exercise level1

---
+++ Python/day 2/variable.py
@@ -0,0 +1,12 @@
+#Day 2: 30 Days of python programming
+first_name = 'Herpsime'
+last_name = 'medi'
+full_name = 'Herpsimedi'
+country = 'Ethiopia'
+city = 'addis ababa' 
+age = 812
+year = 2026
+is_married = False
+is_true = True
+is_light_on = False
+name, age, country = 'Herpsime', 812, 'Ethiopia'
