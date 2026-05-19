a4b13de85edbefed50df3ed503da2c0705a487e6
Herpsime <negarse63@gmail.com>
2026-05-19 18:06:47 +0300

    did till eightth question

---
+++ Python/day 3/exercise3.py
@@ -0,0 +1,26 @@
+age = 265
+
+height = 2.45
+
+z = 2-3j
+
+base = input("Enter Base:")
+height = input("Enter height:")
+area = 0.5 * base * height
+
+a = input("Enter side a:")
+b = input("Enter side b:")
+c = input("Enter side c:")
+
+perimeter = a + b + c
+
+width = input("Enter width:")
+length = input("Enter length:")
+area = length * width
+perimeter = 2 * (length + width)
+
+pi = 3.14
+r = input("Enter radius:")
+area = pi * r ** 2
+c = 2 * pi * r
+
