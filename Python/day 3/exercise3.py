00393b46861050dce0b0692f203b527f73421422
Herpsime <negarse63@gmail.com>
2026-05-30 23:53:46 +0300

    done 8 & 9

--- Python/day 3/exercise3.py
+++ Python/day 3/exercise3.py
@@ -24,3 +24,17 @@ r = input("Enter radius:")
 area = pi * r ** 2
 c = 2 * pi * r
 
+m = 2
+b = -2
+x_int = -b / m
+y_int = b
+slope = m
+print("slope, x-intercept, y-intercept:", slope, (x_int,0), (0,y_int))
+
+x1 = 2
+y1= 2 
+x2 = 6 
+y2 = 10
+m = (y2-y1)/(x2-x1)
+z = ((y2-y1)**2 + (x2-x1)**2)**0.5
+print("slope, euclidean distance:", m, z)
