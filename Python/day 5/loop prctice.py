485acecc40154f4617d3da6764d951422ec37dd0
Herpsime <negarse63@gmail.com>
2026-06-21 03:58:30 +0300

    practiced loop

---
+++ Python/day 5/loop prctice.py
@@ -0,0 +1,18 @@
+#challenge 1 
+for x in range(1,11):
+    print(x)
+#challenge 2
+for x in reversed(range(1,11)):
+    print(x)
+#challenge 3
+num = int(input("eneter number:- "))
+
+for x in range(1,num+1):
+    print(x) 
+
+
+#challenge 4
+i=0
+while i<5:
+    print("*", end="")
+    i+=1
