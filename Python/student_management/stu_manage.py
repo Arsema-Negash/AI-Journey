89c85d5c6ccf46563ac0011e85c348973c994489
Herpsime <negarse63@gmail.com>
2026-06-29 16:05:02 +0300

    asking the user to choose

---
+++ Python/student_management/stu_manage.py
@@ -0,0 +1,10 @@
+print("student management")
+print("1. add student ")
+print("2. view student ")
+print("3. search student ")
+print("4. delete student ")
+print("5. exit ")
+    
+    
+choice = int(input ("enter your choice"))
+        
