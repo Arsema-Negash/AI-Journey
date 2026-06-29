1b0eafcf1dc513453e0d766d4d26b5afaa3bc297
Herpsime <negarse63@gmail.com>
2026-06-29 16:10:30 +0300

    done an add function for the user to add student record

--- Python/student_management/stu_manage.py
+++ Python/student_management/stu_manage.py
@@ -1,3 +1,22 @@
+def add():
+    name=input("enter Name") 
+    ID=input("enter ID") 
+    department=input("enter Department") 
+    Age=input("enter Age") 
+    CGPA=input("enter CGPA") 
+    
+    student={
+        "name":name,
+        "ID":ID,
+        "department":department,
+        "Age":Age,
+        "CGPA":CGPA
+    }
+    
+    return student
+
+
+student={}
 print("student management")
 print("1. add student ")
 print("2. view student ")
