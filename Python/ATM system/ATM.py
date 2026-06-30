3978f347ce815fd9eb3943150c0168aff4716e9e
Herpsime <negarse63@gmail.com>
2026-06-30 14:17:50 +0300

    added a function for deposit

--- Python/ATM system/ATM.py
+++ Python/ATM system/ATM.py
@@ -3,13 +3,22 @@ def deposit():
     amount = float(input("enter amount you want to deposit "))
     if amount >= 0:
         balance += amount
-        print("amount deposited successfully")
+        print(amount,"deposited successfully and now your balance is ",balance)
     else :
         print("invalid amount")
         
     return balance
 
-print("1. deposit")
+def withdraw(balance):
+    amount = float(input("enter amount you want to withdraw "))
+    if amount <= balance:
+        balance -= amount
+        print(amount,"withdrawn successfully")
+    else:
+        print("insufficient balance")
+    return balance
+
+print("1. deposit") 
 print("2. withdraw")
 print("3. check balance")
 print("4. transfer")
