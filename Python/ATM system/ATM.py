00bcd9b3620e30871af6f8b3701c55aa1c6b89b5
Herpsime <negarse63@gmail.com>
2026-06-30 14:09:03 +0300

    added a function deposit so a user can deposit and verify it's not negative amount

--- Python/ATM system/ATM.py
+++ Python/ATM system/ATM.py
@@ -1,6 +1,18 @@
+def deposit():
+    balance = 0
+    amount = float(input("enter amount you want to deposit "))
+    if amount >= 0:
+        balance += amount
+        print("amount deposited successfully")
+    else :
+        print("invalid amount")
+        
+    return balance
+
 print("1. deposit")
 print("2. withdraw")
 print("3. check balance")
 print("4. transfer")
 print("5. exit")
 choice = int(input("enter your choice "))
+
